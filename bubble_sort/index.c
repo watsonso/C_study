79a041806df3b6c3e3194778433b40fd87f9d903
鈴木康文 <uni0360@gmail.com>
2018-03-29 00:10:23 +0900

    complete bubble sort

--- bubble_sort/index.c
+++ bubble_sort/index.c
@@ -2,10 +2,44 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define	N 10 /* データの件数 */
+#define N 10 /* データの件数 */
 int sort[N];
 
 void BubbleSort(void) {
-	int i, j, flag
+    int i, j, flag;
+
+    do {
+        flag = 0;
+        for (i = 0; i < N - 1; i++) {
+            if (sort[i] > sort[i + 1]) {
+                flag = 1;
+                j = sort[1];
+                sort[i] = sort[i + 1];
+                sort[i + 1] = j;
+            }
+        }
+    } while (flag == 1);
 }
 
+int main(void) {
+    int i;
+
+    srand((unsigned int)time(NULL));
+
+    printf("prepare sort：\n");
+    for (i = 0; i < N; i++) {
+        sort[i] = rand();
+        printf("%d ", sort[i]);
+    }
+
+    printf("\n start sort：\n");
+    BubbleSort();
+
+    printf("\n end sort：\n");
+
+    for (i = 0; i < N; i++) {
+        printf("%d ", sort[i]);
+    }
+
+    return EXIT_SUCCESS;
+}
